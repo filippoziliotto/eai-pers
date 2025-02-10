a149750606d8f12c4b27ee6cd0bcc6b4b8a2cf20
filippoziliotto <filippo.ziliotto@phd.unipd.it>
2025-02-10 09:40:03 +0100

    add description aug

--- dataset/transform.py
+++ dataset/transform.py
@@ -1,12 +1,12 @@
+
 # Custom Transform
 import torch
-import args  # Import the arguments
-
+import args
+import random
 
 # Import utility functions
 from dataset.utils import random_crop_preserving_target
    
-
 class MapTransform:
     """
     Custom transformation class for maps and their corresponding descriptions.
@@ -28,23 +28,25 @@ class MapTransform:
         if not self.use_aug:
             return feature_map, xy_coords, description
                 
-        # Apply horizontal flip
+        # Apply horizontal flip (1x more likely)
         if self.use_horizontal_flip and torch.rand(1) > self.prob:
             feature_map = torch.flip(feature_map, dims=(1,))
             xy_coords[0] = feature_map.shape[1] - xy_coords[0]
             
-        # Apply vertical flip
+        # Apply vertical flip (1x more likely)
         if self.use_vertical_flip and torch.rand(1) > self.prob:
             feature_map = torch.flip(feature_map, dims=(0,))
             xy_coords[1] = feature_map.shape[0] - xy_coords[1]
         
-        # Apply random crop
-        if self.use_random_crop and torch.rand(1) > self.prob/2:
+        # Apply random crop (0.5x more likely)
+        if self.use_random_crop and torch.rand(1) > self.prob*2:
             feature_map, xy_coords = random_crop_preserving_target(feature_map, xy_coords)
         
-        # Change in description order   
-        if self.use_desc_aug and torch.rand(1) > self.prob:
-            # TODO:
-            pass 
+        # Change in description order (2x more likely)
+        if self.use_desc_aug and torch.rand(1) > self.prob/2:
+            # Randomly change the order of the single desciptions
+            desc_list = description.split(".")
+            random.shuffle(desc_list)
+            description = ". ".join(desc_list)
         
         return feature_map, xy_coords, description
